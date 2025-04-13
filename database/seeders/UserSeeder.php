5cb1d36c135fa92bc5e7e40f354c38b89c2b487e
Leandro <leandrodev83@gmail.com>
2025-04-13 00:30:20 -0300

    cria seeder user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Leandro Carvalho',
+                'username' => 'lrflix',
+                'email' => 'adm@gmail.com',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'Vendedor vendor',
+                'username' => 'vendor',
+                'email' => 'vendedor@gmail.com',
+                'role' => 'vendor',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'Cliente user',
+                'username' => 'user',
+                'email' => 'cliente@gmail.com',
+                'role' => 'user',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ]
+        ]);
+    }
+}
